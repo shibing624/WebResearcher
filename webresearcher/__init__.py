5090732b0d73aa547e6c23e18fe60d1dab84deb5
shibing624 <shibing624@126.com>
2025-10-26 22:39:01 +0800

    version 0.2.0

--- webresearcher/__init__.py
+++ webresearcher/__init__.py
@@ -8,7 +8,7 @@ featuring unbounded reasoning capability through iterative synthesis.
 @author: XuMing(xuming624@qq.com)
 """
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 __author__ = "XuMing"
 __email__ = "xuming624@qq.com"
 __url__ = "https://github.com/shibing624/WebResearcher"
